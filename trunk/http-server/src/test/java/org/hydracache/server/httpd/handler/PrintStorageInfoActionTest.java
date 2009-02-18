125b6848c67ae17fe716104bc7dd6ac5b0e09218
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2009-02-18 21:39:47 +0000

    RESOLVED - issue HYDRACACHE-7: Multiple put and get operations through client library stalls http://jira.codehaus.org/browse/HYDRACACHE-7

--- trunk/http-server/src/test/java/org/hydracache/server/httpd/handler/PrintStorageInfoActionTest.java
+++ trunk/http-server/src/test/java/org/hydracache/server/httpd/handler/PrintStorageInfoActionTest.java
@@ -1,8 +1,13 @@
 package org.hydracache.server.httpd.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.math.RandomUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.entity.StringEntity;
 import org.hydracache.server.data.resolver.ArbitraryResolver;
+import org.hydracache.server.data.storage.Data;
 import org.hydracache.server.data.storage.DataBank;
 import org.hydracache.server.data.storage.EhcacheDataBank;
 import org.jmock.Expectations;
@@ -13,7 +18,36 @@ public class PrintStorageInfoActionTest {
     private Mockery context = new Mockery();
 
     @Test
+    public void ensurePrintDataSetCanHandleNulls() throws Exception {
+        DataBank dataBank = new EhcacheDataBank(new ArbitraryResolver());
+        
+        PrintStorageInfoAction handler = new PrintStorageInfoAction(dataBank);
+
+        List<Data> dataSet = new ArrayList<Data>();
+        
+        dataSet.add(null);
+        dataSet.add(new Data(RandomUtils.nextLong()));
+        
+        handler.printDataSet(dataSet);
+    }
+
+    @Test
     public void ensureCorrectStoragePrint() throws Exception {
+        final HttpResponse response = mockHttpResponse();
+
+        DataBank dataBank = new EhcacheDataBank(new ArbitraryResolver());
+        
+        dataBank.put(new Data(RandomUtils.nextLong()));
+        dataBank.put(new Data(RandomUtils.nextLong()));
+
+        PrintStorageInfoAction handler = new PrintStorageInfoAction(dataBank);
+
+        handler.execute(response);
+
+        context.assertIsSatisfied();
+    }
+
+    private HttpResponse mockHttpResponse() {
         final HttpResponse response = context.mock(HttpResponse.class);
 
         context.checking(new Expectations() {
@@ -21,14 +55,7 @@ public class PrintStorageInfoActionTest {
                 one(response).setEntity(with(any(StringEntity.class)));
             }
         });
-
-        DataBank dataBank = new EhcacheDataBank(new ArbitraryResolver());
-
-        PrintStorageInfoAction handler = new PrintStorageInfoAction(dataBank);
-        
-        handler.execute(response);
-
-        context.assertIsSatisfied();
+        return response;
     }
 
 }
