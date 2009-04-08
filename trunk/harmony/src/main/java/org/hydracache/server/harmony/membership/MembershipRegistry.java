e36ce38916c856445a3c2a8100c3a6003df13515
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2009-04-08 21:34:54 +0000

    Added neighbor lookup in the membership registry class

--- trunk/harmony/src/main/java/org/hydracache/server/harmony/membership/MembershipRegistry.java
+++ trunk/harmony/src/main/java/org/hydracache/server/harmony/membership/MembershipRegistry.java
@@ -1,15 +1,27 @@
 package org.hydracache.server.harmony.membership;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
+import org.apache.commons.lang.Validate;
+import org.hydracache.server.Identity;
 import org.hydracache.server.harmony.core.Node;
 import org.hydracache.server.harmony.core.NodeSet;
+import org.hydracache.server.harmony.core.Space;
 
 public class MembershipRegistry {
     protected ConcurrentSkipListSet<Node> registry = new ConcurrentSkipListSet<Node>();
 
+    private ConcurrentHashMap<Identity, Boolean> neighborLookup = new ConcurrentHashMap<Identity, Boolean>();
+
+    private Space space;
+
     public MembershipRegistry(Node self) {
         registry.add(self);
+    }
+
+    public void setSpace(Space space) {
+        this.space = space;
     }
 
     public NodeSet listAllMembers() {
@@ -18,10 +30,12 @@ public class MembershipRegistry {
 
     public void register(Node node) {
         registry.add(node);
+        neighborLookup.clear();
     }
 
     public void deregister(Node node) {
         registry.remove(node);
+        neighborLookup.clear();
     }
 
     public boolean contains(Node node) {
@@ -32,4 +46,17 @@ public class MembershipRegistry {
         return registry.size();
     }
 
+    public boolean isNeigbor(Node node) {
+        Validate.notNull(node, "Node can not be null");
+
+        Boolean isNeighbor = neighborLookup.get(node.getId());
+
+        if (isNeighbor == null) {
+            isNeighbor = space.findSubstancesForLocalNode().isNeighbor(
+                    node.getId());
+            neighborLookup.putIfAbsent(node.getId(), isNeighbor);
+        }
+
+        return isNeighbor;
+    }
 }
--- trunk/harmony/src/test/java/org/hydracache/server/harmony/membership/MembershipRegistryTest.java
+++ trunk/harmony/src/test/java/org/hydracache/server/harmony/membership/MembershipRegistryTest.java
@@ -1,21 +1,153 @@
 package org.hydracache.server.harmony.membership;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.hydracache.server.Identity;
 import org.hydracache.server.harmony.core.Node;
 import org.hydracache.server.harmony.core.NodeSet;
+import org.hydracache.server.harmony.core.Space;
+import org.hydracache.server.harmony.core.SubstanceSet;
 import org.hydracache.server.harmony.jgroups.JGroupsNode;
 import org.jgroups.stack.IpAddress;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 
 public class MembershipRegistryTest {
 
+    private Mockery context = new Mockery() {
+        {
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }
+    };
+
     private Node nodeA = new JGroupsNode(new Identity(8080), new IpAddress());
     private Node nodeA2 = new JGroupsNode(new Identity(8080), new IpAddress());
     private Node nodeB = new JGroupsNode(new Identity(8081), new IpAddress());
+    private Node nodeC = new JGroupsNode(new Identity(8082), new IpAddress());
+
+    @Test
+    public void ensureNeighborInfoCacheIsInvalidatedAfterDeregister() {
+        MembershipRegistry registry = new MembershipRegistry(nodeA);
+
+        final SubstanceSet substanceSet = context.mock(SubstanceSet.class);
+
+        {
+            addNodeBIsNeighborExp(substanceSet);
+            addNodeBIsNeighborExp(substanceSet);
+            addNodeCIsNotNeighborExp(substanceSet);
+            addNodeCIsNotNeighborExp(substanceSet);
+        }
+
+        final Space space = context.mock(Space.class);
+
+        {
+            addFindSubstancesForLocalNodeExp(substanceSet, space);
+        }
+
+        registry.setSpace(space);
+
+        assertTrue("Should be a neigbor", registry.isNeigbor(nodeB));
+        assertFalse("Should not be a neigbor", registry.isNeigbor(nodeC));
+
+        registry.deregister(nodeA2);
+
+        assertTrue("Should be a neigbor", registry.isNeigbor(nodeB));
+        assertFalse("Should not be a neigbor", registry.isNeigbor(nodeC));
+
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void ensureNeighborInfoCacheIsInvalidatedAfterRegister() {
+        MembershipRegistry registry = new MembershipRegistry(nodeA);
+
+        final SubstanceSet substanceSet = context.mock(SubstanceSet.class);
+
+        {
+            addNodeBIsNeighborExp(substanceSet);
+            addNodeBIsNeighborExp(substanceSet);
+            addNodeCIsNotNeighborExp(substanceSet);
+            addNodeCIsNotNeighborExp(substanceSet);
+        }
+
+        final Space space = context.mock(Space.class);
+
+        {
+            addFindSubstancesForLocalNodeExp(substanceSet, space);
+        }
+
+        registry.setSpace(space);
+
+        assertTrue("Should be a neigbor", registry.isNeigbor(nodeB));
+        assertFalse("Should not be a neigbor", registry.isNeigbor(nodeC));
+
+        registry.register(nodeA2);
+
+        assertTrue("Should be a neigbor", registry.isNeigbor(nodeB));
+        assertFalse("Should not be a neigbor", registry.isNeigbor(nodeC));
+
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void ensureNeighborInfoIsCaculatedAndCached() {
+        MembershipRegistry registry = new MembershipRegistry(nodeA);
+
+        final SubstanceSet substanceSet = context.mock(SubstanceSet.class);
+
+        {
+            addNodeBIsNeighborExp(substanceSet);
+            addNodeCIsNotNeighborExp(substanceSet);
+        }
+
+        final Space space = context.mock(Space.class);
+
+        {
+            addFindSubstancesForLocalNodeExp(substanceSet, space);
+        }
+
+        registry.setSpace(space);
+
+        assertTrue("Should be a neigbor", registry.isNeigbor(nodeB));
+        assertTrue("Should be a neigbor again", registry.isNeigbor(nodeB));
+        assertFalse("Should not be a neigbor", registry.isNeigbor(nodeC));
+        assertFalse("Should not be a neigbor again", registry.isNeigbor(nodeC));
+
+        context.assertIsSatisfied();
+    }
+
+    private void addNodeBIsNeighborExp(final SubstanceSet substanceSet) {
+        context.checking(new Expectations() {
+            {
+                one(substanceSet).isNeighbor(nodeB.getId());
+                will(returnValue(true));
+            }
+        });
+    }
+
+    private void addNodeCIsNotNeighborExp(final SubstanceSet substanceSet) {
+        context.checking(new Expectations() {
+            {
+                one(substanceSet).isNeighbor(nodeC.getId());
+                will(returnValue(false));
+            }
+        });
+    }
+
+    private void addFindSubstancesForLocalNodeExp(
+            final SubstanceSet substanceSet, final Space space) {
+        context.checking(new Expectations() {
+            {
+                atLeast(1).of(space).findSubstancesForLocalNode();
+                will(returnValue(substanceSet));
+            }
+        });
+    }
 
     @Test
     public void ensureInitialNodeSetIsNotEmpty() {
@@ -54,19 +186,19 @@ public class MembershipRegistryTest {
         MembershipRegistry registry = new MembershipRegistry(nodeA);
 
         registry.deregister(nodeA);
-        
+
         NodeSet nodes = registry.listAllMembers();
 
         assertEquals("Initial node set is not empty", 0, nodes.size());
     }
-    
+
     @Test
     public void ensureNodeCanBeDeregisteredMulitpleTimes() {
         MembershipRegistry registry = new MembershipRegistry(nodeA);
 
         registry.deregister(nodeA);
         registry.deregister(nodeA);
-        
+
         NodeSet nodes = registry.listAllMembers();
 
         assertEquals("Initial node set is not empty", 0, nodes.size());
