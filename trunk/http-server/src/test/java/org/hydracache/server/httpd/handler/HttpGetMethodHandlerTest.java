75ab2030a04420f46075875c91534a9eccfd1b2f
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2010-02-25 04:32:03 +0000

    refactored unit test to use mockito instead of jmock, and added a new test case for parameterized protocol request

--- trunk/http-server/src/test/java/org/hydracache/server/httpd/handler/HttpGetMethodHandlerTest.java
+++ trunk/http-server/src/test/java/org/hydracache/server/httpd/handler/HttpGetMethodHandlerTest.java
@@ -1,109 +1,153 @@
 package org.hydracache.server.httpd.handler;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.RequestLine;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HttpContext;
+import org.hydracache.data.hashing.HashFunction;
+import org.hydracache.data.hashing.KetamaBasedHashFunction;
 import org.hydracache.protocol.data.codec.DefaultProtocolEncoder;
 import org.hydracache.protocol.data.marshaller.MessageMarshallerFactory;
-import org.jmock.Expectations;
+import org.hydracache.server.Identity;
+import org.hydracache.server.IdentityMarshaller;
+import org.hydracache.server.data.storage.Data;
+import org.hydracache.server.data.versioning.IncrementVersionFactory;
+import org.hydracache.server.harmony.storage.HarmonyDataBank;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-public class HttpGetMethodHandlerTest extends AbstractHttpMethodHandlerTest {
-    private HttpGetMethodHandler handler;
+public class HttpGetMethodHandlerTest {
+    @Mock
+    private HarmonyDataBank mockDataBank;
+    @Mock
+    private HttpRequest mockRequest;
+    @Mock
+    private HttpResponse mockResponse;
+    @Mock
+    private HttpContext mockHttpContext;
 
-    @Override
-    public void initialize() {
-        super.initialize();
-
-        createHandler();
+    public HttpGetMethodHandlerTest() {
     }
 
-    private void createHandler() {
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void ensureHandleGetXmlDataCorrectly() throws HttpException,
+            IOException {
+
+        HttpGetMethodHandler handler = createHttpGetMethodHandler();
+
+        stubGetRequestURI(mockRequest, "/testContext/testKey?protocol=xml");
+        stubSuccessfulReliableGet(mockDataBank);
+
+        handler.handle(mockRequest, mockResponse, mockHttpContext);
+
+        verify(mockResponse).setStatusCode(HttpStatus.SC_OK);
+        verify(mockResponse).setEntity(any(StringEntity.class));
+    }
+
+    private HttpGetMethodHandler createHttpGetMethodHandler() {
+        HashFunction hashFunction = new KetamaBasedHashFunction();
+
+        IncrementVersionFactory versionFactoryMarshaller = new IncrementVersionFactory(
+                new IdentityMarshaller());
+
         DefaultProtocolEncoder messageEncoder = new DefaultProtocolEncoder(
                 new MessageMarshallerFactory(versionFactoryMarshaller));
 
-        handler = new HttpGetMethodHandler(dataBank, hashFunction,
-                messageEncoder);
+        HttpGetMethodHandler handler = new HttpGetMethodHandler(mockDataBank,
+                hashFunction, messageEncoder);
+
+        return handler;
+    }
+
+    private void stubSuccessfulReliableGet(HarmonyDataBank mockDataBank)
+            throws IOException {
+        Data data = new Data();
+        data.setVersion(new IncrementVersionFactory().create(new Identity(70)));
+        when(mockDataBank.get(anyString(), anyLong())).thenReturn(data);
+    }
+
+    private void stubGetRequestURI(HttpRequest mockRequest, String requestUri) {
+        RequestLine mockRequestLine = mock(RequestLine.class);
+        when(mockRequest.getRequestLine()).thenReturn(mockRequestLine);
+        when(mockRequestLine.getUri()).thenReturn(requestUri);
     }
 
     @Test
     public void ensureServiceActionExecution() throws Exception {
         Set<HttpServiceAction> actions = new HashSet<HttpServiceAction>();
 
-        HttpServiceAction mockAction = context.mock(HttpServiceAction.class);
+        HttpServiceAction mockAction = mock(HttpServiceAction.class);
 
-        {
-            addGetNameExp(mockAction);
-            addExecuteExp(mockAction);
-        }
+        when(mockAction.getName()).thenReturn("mock");
 
         actions.add(mockAction);
 
+        HttpGetMethodHandler handler = createHttpGetMethodHandler();
+
         handler.setServiceActions(actions);
 
-        {
-            addGetRequestLineExp(request, "/mock/");
-        }
+        stubGetRequestURI(mockRequest, "/mock");
 
-        handler.handle(request, response, httpContext);
+        handler.handle(mockRequest, mockResponse, mockHttpContext);
+
+        verify(mockAction).execute(mockResponse);
     }
 
     @Test
     public void ensureHandleGetDataCorrectly() throws HttpException,
             IOException {
-        {
-            addGetRequestLineExp(request, "/testContext/testKey/");
-        }
+        stubGetRequestURI(mockRequest, "/testContext/testKey");
+        stubSuccessfulReliableGet(mockDataBank);
 
-        {
-            addSuccessfulReliableGetExp(dataBank, testStorageContext);
-        }
+        HttpGetMethodHandler handler = createHttpGetMethodHandler();
 
-        {
-            addSetBinaryDataEntityExp(response);
-            addSetOkStatusCodeExp(response);
-        }
+        handler.handle(mockRequest, mockResponse, mockHttpContext);
 
-        handler.handle(request, response, httpContext);
+        verify(mockResponse).setStatusCode(HttpStatus.SC_OK);
+        verify(mockResponse).setEntity(any(ByteArrayEntity.class));
     }
 
     @Test
     public void ensureNotFoundGenerates404() throws Exception {
-        {
-            addGetRequestLineExp(request, "/testContext/testKey/");
-        }
+        stubGetRequestURI(mockRequest, "/testContext/testKey");
 
-        {
-            addNotFoundReliableGetExp(dataBank, testStorageContext);
-        }
+        HttpGetMethodHandler handler = createHttpGetMethodHandler();
 
-        {
-            addSetStringMessageEntityExp(response);
-            addSetNotFoundStatusCodeExp(response);
-        }
+        handler.handle(mockRequest, mockResponse, mockHttpContext);
 
-        handler.handle(request, response, httpContext);
-    }
-
-    protected void addSetNotFoundStatusCodeExp(final HttpResponse response) {
-        context.checking(new Expectations() {
-            {
-                one(response).setStatusCode(HttpStatus.SC_NOT_FOUND);
-            }
-        });
+        verify(mockResponse).setStatusCode(HttpStatus.SC_NOT_FOUND);
+        verify(mockResponse).setEntity(any(StringEntity.class));
     }
 
     @Test
     public void ensureBlankKeyIsRejected() throws HttpException, IOException {
-        {
-            addGetRequestLineExp(request, "/");
-        }
+        stubGetRequestURI(mockRequest, "/");
 
-        handler.handle(request, response, httpContext);
+        HttpGetMethodHandler handler = createHttpGetMethodHandler();
+
+        handler.handle(mockRequest, mockResponse, mockHttpContext);
     }
 
 }
