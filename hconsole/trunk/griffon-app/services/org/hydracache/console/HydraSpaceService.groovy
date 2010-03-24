64e476234e420cfcd281d678eda2b38425354d96
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2010-03-24 16:33:08 +0000

    added null guards

--- hconsole/trunk/griffon-app/services/org/hydracache/console/HydraSpaceService.groovy
+++ hconsole/trunk/griffon-app/services/org/hydracache/console/HydraSpaceService.groovy
@@ -1,12 +1,34 @@
 package org.hydracache.console
 
+import org.hydracache.client.HydraCacheAdminClient
+import org.hydracache.client.HydraCacheClientFactory
+import org.hydracache.server.Identity
+
 class HydraSpaceService {
+    public static final String HYDRA_SPACE_CONNECTED_EVENT = "HydraSpaceConnected"
+
+    def hydraCacheClientFactory = new HydraCacheClientFactory()
+
     def connect(server, port) {
+        if (!server || !port)
+            return false
+
         log.debug "Connecting to ${server}:${port} ..."
 
-        app.event("HydraSpaceConnected")
+        HydraCacheAdminClient adminClient = hydraCacheClientFactory.createAdminClient(
+                [new Identity(Inet4Address.getByName(server), Integer.valueOf(port))]
+        )
+
+        def nodes = adminClient.listNodes()
+
+        if (!nodes)
+            return false
+
+        app.event(HYDRA_SPACE_CONNECTED_EVENT)
 
         log.debug "[HydraSpaceConnected] event sent"
+
+        return true
     }
 
 }
--- hconsole/trunk/test/unit/org/hydracache/console/HydraSpaceServiceTests.groovy
+++ hconsole/trunk/test/unit/org/hydracache/console/HydraSpaceServiceTests.groovy
@@ -1,8 +1,46 @@
 package org.hydracache.console
 
+import org.apache.log4j.Logger
+import org.hydracache.client.HydraCacheAdminClient
+import org.hydracache.server.Identity
+
 class HydraSpaceServiceTests extends GroovyTestCase {
+    HydraSpaceService service = new HydraSpaceService()
+    def events = [:]
 
-    void testSomething() {
-
+    void setUp() {
+        service.metaClass.log = Logger.getLogger(HydraSpaceService)
+        service.metaClass.app = [event: {events[it] = []}]
     }
+
+    void tearDown() {
+        events.clear()
+    }
+
+    void testSuccessfulConnection() {
+        def stubAdminClient = [listNodes: {[new Identity(80)]}] as HydraCacheAdminClient
+
+        service.hydraCacheClientFactory = [createAdminClient: {stubAdminClient}]
+
+        def result = service.connect("localhost", "8888")
+
+        assertTrue "Connection should be successful", result
+        assertTrue "Event ${HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT} should be sent", events.containsKey(HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT)
+    }
+
+    void testServerNullHandlingInConnect() {
+        def result = service.connect(null, "8888")
+
+        assertFalse "Connection should not be successful", result
+        assertFalse "Event ${HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT} should not be sent", events.containsKey(HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT)
+    }
+
+    void testPortNullHandlingInConnect() {
+        def result = service.connect("server", null)
+
+        assertFalse "Connection should not be successful", result
+        assertFalse "Event ${HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT} should not be sent", events.containsKey(HydraSpaceService.HYDRA_SPACE_CONNECTED_EVENT)
+    }
+
+    
 }
