1a724bd981b7fbb2ff210acc6447db3bfb073a1a
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2010-03-26 21:33:48 +0000

    added test case for connect

--- hconsole/trunk/test/unit/org/hydracache/console/AddressBarControllerTests.groovy
+++ hconsole/trunk/test/unit/org/hydracache/console/AddressBarControllerTests.groovy
@@ -1,13 +1,14 @@
 package org.hydracache.console
 
 import org.hydracache.console.validate.Errors
+import groovy.mock.interceptor.*
 
 /**
  * Created by nick.zhu
  */
 class AddressBarControllerTests extends GriffonTestCase {
 
-    public void testConnectValidationFailure(){
+    public void testConnectValidationFailure() {
         def controller = new AddressBarController()
 
         def expectedErrors = new Errors()
@@ -15,12 +16,35 @@ class AddressBarControllerTests extends GriffonTestCase {
 
         mockController(controller)
 
-        controller.model = [validate:{false}, errors:expectedErrors]
-        controller.view = [errorMessagePanel:errorMsgPanel]
+        controller.model = [validate: {false}, errors: expectedErrors]
+        controller.view = [errorMessagePanel: errorMsgPanel]
 
         controller.connect()
 
         assertEquals "Errors were not generated", expectedErrors, errorMsgPanel.errors
+    }
+
+    public void testConnectSuccess() {
+        def controller = new AddressBarController()
+
+        def errorMsgPanel = [:]
+        def mockHydraServiceContext = new MockFor(HydraSpaceService)
+        mockHydraServiceContext.demand.connect(1){
+            server, port ->
+            assertEquals("server", server)
+            assertEquals("90", port)
+            true
+        }
+
+        mockController(controller)
+
+        controller.model = [validate: {true}, server: "server", port: "90"]
+        controller.view = [errorMessagePanel: errorMsgPanel]
+        controller.hydraSpaceService = mockHydraServiceContext.proxyInstance()
+
+        controller.connect()
+
+        mockHydraServiceContext.verify controller.hydraSpaceService
     }
 
 }
