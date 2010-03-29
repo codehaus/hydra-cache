5cf92c38a8cee8e096a2afa2c297a71e9ad51b26
aucifer_nick <aucifer_nick@8800a6ce-cf61-0410-999d-ef38c60c1380>
2010-03-29 15:49:17 +0000

    removed validation runtime enhancement

--- hconsole/trunk/griffon-app/conf/Events.groovy
+++ hconsole/trunk/griffon-app/conf/Events.groovy
@@ -1,19 +1,10 @@
 import org.apache.log4j.Logger
-import org.hydracache.console.validate.ValidationEnhancer
 
 onNewInstance = {klass, type, instance ->
     injectLog(klass, instance)
-
-    if (type == "model") {
-        enhanceModelWithValidationSupport(instance)
-    }
 }
 
 private def injectLog(klass, instance) {
     instance.metaClass.log = Logger.getLogger(klass)
-}
-
-private def enhanceModelWithValidationSupport(instance) {
-    new ValidationEnhancer(instance)
 }
 
